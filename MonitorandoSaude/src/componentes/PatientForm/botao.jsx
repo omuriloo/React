83024242e5f03689c6999f5d8cc7aa8a68d16a0d
omuriloo <murilorihan@gmail.com>
2025-05-13 21:40:09 -0300

    monitorando saude

---
+++ MonitorandoSaude/src/componentes/PatientForm/botao.jsx
@@ -0,0 +1,7 @@
+function botao() {
+    return (
+      <a href="https://www.exemplo.com" target="_blank" rel="noopener noreferrer">
+        <button>Clique aqui</button>
+      </a>
+    );
+  }
--- ProjetoVite/src/assets/componentes/Botao/Botao.jsx
+++ ProjetoVite/src/assets/componentes/Botao/Botao.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import './Botao.css'
 
 const Botao = () => {
-    // const handleClick = () => {
-    //  <a  href=""></a>
 
-    // }
     return(
-        //<button className="botao" onClick={"youtu.be/nbtmsJQQpAM?si=-nFkBpnLPSgi5lAo"}>Latidos</button>
         <a href="https://www.youtube.com/watch?v=nbtmsJQQpAM" target="_blank" rel="noopener noreferrer">
             <button type="button">youtube</button>
         </a>
